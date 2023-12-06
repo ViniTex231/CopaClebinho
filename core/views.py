24c537819bf2b71ccca785e49dc0372a00bb2b6e
ViniTex231 <viniteixeira2828@gmail.com>
2023-12-06 16:48:14 -0300

    db

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import *
 from .serializers import *
@@ -16,6 +15,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
+            
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
